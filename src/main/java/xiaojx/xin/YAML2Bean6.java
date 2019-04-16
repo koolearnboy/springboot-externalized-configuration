e97dfa0e13c438975194ec453e06de57c6da0c9d
Michaeljx <koolearnboy@gmail.com>
2019-04-16 21:05:05 +0800

    1、配置文件可以放在4个地方，他们优先级分别不同 2、可以通过@Value将配置文件的值直接绑定到字段上，也可以在class上使用@ConfigurationProperties注解，直接将配置文件的值     绑定到该class的Bean中，并放入到容器中 3、配置文件可以通过使用 "---" 进行分割不同的profile，在不同的profile下可以配置不同的属性，默认激活的诗default

--- src/main/java/xiaojx/xin/YAML2Bean6.java
+++ src/main/java/xiaojx/xin/YAML2Bean6.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -19,15 +21,19 @@ import java.util.Map;
 @ToString
 @Component
 @ConfigurationProperties(prefix = "acme")
+@Validated
 public class YAML2Bean6 {
 
-    private List<MyPOJO> list;
+    private final List<MyPOJO> list = new ArrayList<MyPOJO>();
     private Map<String,MyPOJO> map;
+//    @NotNull
+    private String isNull;
 
     @Getter
     @Setter
     @ToString
-    class MyPOJO{
+    //使用内部类就需要为static 可以直接new MyPOJO对象
+    static class MyPOJO{
         private String name;
         private String desc;
     }
